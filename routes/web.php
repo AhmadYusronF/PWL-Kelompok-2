fe98a10d65bb2be54c3713be03f9fa6e28419476
AhmadYusron <yusronfadilahmad@gmail.com>
2026-05-01 00:52:29 +0700

    Quarto Salato

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,21 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function (){
+Route::get('/about', function () {
     return view('about');
 });
 
-Route::get('/blog', function (){
-    return view('blog');
-});
+Route::get(
+    '/blog',
+    [BlogController::class, 'Blog']
+);
 
-Route::get('/contact', function (){
+Route::get('/contact', function () {
     return view('contact');
 });
